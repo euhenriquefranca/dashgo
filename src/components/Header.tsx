53b726f37ddd3d94a26fdcd7db22bbd2150fb791
Henrique França <euhenriquefranca@gmail.com>
2021-07-29 16:35:35 -0300

    Finished Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Input, Text, Icon } from '@chakra-ui/react';
-import { RiSearchLine } from 'react-icons/ri'
+import { Flex, Input, Text, Icon, HStack, Box, Avatar } from '@chakra-ui/react';
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
 
 export function Header() {
   return (
@@ -20,10 +20,8 @@ export function Header() {
         w="64"
       >
         dashgo
-        <Text
-          as="span"
-          ml="1"
-          color="pink.500">.</Text>
+        <Text as="span" ml="1" color="pink.500">.</Text>
+        
       </Text>
       <Flex
         as="label"
@@ -46,6 +44,32 @@ export function Header() {
           _placeholder={{color: "gray.400"}}
         />
         <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
+
+      <Flex align="center" ml="auto">
+        <HStack
+          spacing="4"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20"/>
+          <Icon as={RiUserAddLine} fontSize="20"/>
+        </HStack>
+
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>Henrique França</Text>
+            <Text color="gray.300">
+              euhenriquefranca@gmail.com
+            </Text>
+          </Box>
+
+          <Avatar size="md" name="Henrique França" src="https://github.com/euhenriquefranca.png" />
+        </Flex>
       </Flex>
     </Flex>
   );
