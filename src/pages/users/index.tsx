a4f8fce9047a25a4f2aa78a3f41c43b15bc6539c
Henrique França <euhenriquefranca@gmail.com>
2021-07-30 17:25:12 -0300

    Component Pagination

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from '../../components/Header'
 import { Sidebar } from '../../components/Sidebar'
+import { Pagination } from "../Pagination";
 
 
 export default function UserList() {
@@ -71,6 +72,7 @@ export default function UserList() {
               </Tr>
             </Tbody>
           </Table>
+          <Pagination />
         </Box>
       </Flex>
     </Box>
