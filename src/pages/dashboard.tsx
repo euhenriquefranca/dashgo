2f98329d4d7f693f1231e740f0982d6df35ba1c9
Henrique França <euhenriquefranca@gmail.com>
2021-07-31 11:59:07 -0300

    Responsive Dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -2,6 +2,7 @@ import { Box, Flex, Text, SimpleGrid, theme } from '@chakra-ui/react';
 import dynamic from 'next/dynamic';
 import { Header } from '../components/Header';
 import { Sidebar } from '../components/Sidebar';
+import {} from 'react-apexcharts'
 
 const Chart = dynamic(() => import('react-apexcharts'), {
   ssr: false,
@@ -54,7 +55,7 @@ const options = {
     },
   },
 };
-const series = [{ name: 'seires1', data: [31, 120, 10, 28, 61, 18, 109] }];
+const series = [{ name: 'series1', data: [31, 120, 10, 28, 61, 18, 109] }];
 
 export default function Dashboard() {
   return (
@@ -65,16 +66,22 @@ export default function Dashboard() {
         <Sidebar />
 
         <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
-          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
-            <Text fontSize="lg" mb="4">
-              Inscritos da semana
-            </Text>
+          <Box
+            p={["6", "8"]}
+            bg="gray.800"
+            borderRadius={8}
+            pb="4"
+          >
+            <Text fontSize="lg" mb="4">Inscritos da semana</Text>
             <Chart options={options} series={series} type="area" height={160} />
           </Box>
-          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
-            <Text fontSize="lg" mb="4">
-              Taxa de abertura
-            </Text>
+          <Box
+            p={["6", "8"]}
+            bg="gray.800"
+            borderRadius={8}
+            pb="4"
+          >
+            <Text fontSize="lg" mb="4">Taxa de abertura</Text>
             <Chart options={options} series={series} type="area" height={160} />
           </Box>
         </SimpleGrid>
