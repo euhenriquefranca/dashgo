dc67bc745d7d83261a80e559be29af62c1ca7a4b
Henrique França <euhenriquefranca@gmail.com>
2021-08-03 12:13:14 -0300

    factories and seeds

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,34 +1,50 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
-import React from "react";
-import { RiAddLine, RiPencilLine } from "react-icons/ri";
-import { Header } from '../../components/Header'
-import { Sidebar } from '../../components/Sidebar'
-import { Pagination } from "../../components/Pagination";
-import Link from "next/link";
-
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+  useBreakpointValue,
+} from '@chakra-ui/react';
+import React, { useEffect } from 'react';
+import { RiAddLine, RiPencilLine } from 'react-icons/ri';
+import { Header } from '../../components/Header';
+import { Sidebar } from '../../components/Sidebar';
+import { Pagination } from '../../components/Pagination';
+import Link from 'next/link';
 
 export default function UserList() {
   const isWideVersion = useBreakpointValue({
     base: false,
-    lg: true
-  })
+    lg: true,
+  });
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/users')
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
 
   return (
     <Box>
       <Header />
 
-      <Flex
-        w="100%"
-        my="6"
-        maxWidth={1480}
-        mx="auto"
-        px="6"
-      >
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
 
         <Box flex="1" borderRadius={8} bg="gray.800" p="8">
           <Flex mb="8" justify="space-between" align="center">
-            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+            </Heading>
 
             <Link href="/users/create" passHref>
               <Button
@@ -46,7 +62,7 @@ export default function UserList() {
           <Table colorScheme="whiteAlpha">
             <Thead>
               <Tr>
-                <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                <Th px={['4', '4', '6']} color="gray.300" width="8">
                   <Checkbox colorScheme="pink" />
                 </Th>
                 <Th>Usuário</Th>
@@ -55,8 +71,8 @@ export default function UserList() {
             </Thead>
             <Tbody>
               <Tr>
-                <Td px={["4", "4", "6"]}>
-                 <Checkbox colorScheme="pink" />
+                <Td px={['4', '4', '6']}>
+                  <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
                   <Box>
@@ -65,11 +81,10 @@ export default function UserList() {
                   </Box>
                 </Td>
                 {isWideVersion && <Td>04 de Abril, 2021</Td>}
-                
               </Tr>
               <Tr>
-                <Td px={["4", "4", "6"]}>
-                 <Checkbox colorScheme="pink" />
+                <Td px={['4', '4', '6']}>
+                  <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
                   <Box>
@@ -78,7 +93,6 @@ export default function UserList() {
                   </Box>
                 </Td>
                 {isWideVersion && <Td>04 de Abril, 2021</Td>}
-                
               </Tr>
             </Tbody>
           </Table>
@@ -86,5 +100,5 @@ export default function UserList() {
         </Box>
       </Flex>
     </Box>
-  )
+  );
 }
